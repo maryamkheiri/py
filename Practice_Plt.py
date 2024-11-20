105800e86e5f6cd6df344991a84888eb02d4fd99
maryamkheiri <maryakh58@gmail.com>
2024-11-20 20:16:28 +0330

    Practice_matplotlib

---
+++ Practice_Plt.py
@@ -0,0 +1,67 @@
+#draw (plot $ bar $ scatter $pie $donut )graphs in matplotlib
+import matplotlib.pyplot as plt
+from random import randint as rnd
+x=list(range(50))
+y=[rnd(-10,10) for i in range(50)]
+z=[rnd(-5,10) for i in range(50)]
+c=("red")
+plt.figure(figsize=(20,5))
+plt.title("Gas Prices\n-10,10",color="purple",fontsize = 22)
+plt.xlabel("Time" ,color="purple")
+plt.ylabel("Raised_Price" , color="purple")
+plt.plot(x,y,color=c, label="Gas")
+plt.plot(x,z,color="green", label="Oil")
+plt.grid()
+plt.legend()
+plt.show()
+
+print("######")
+x=list(range(50))
+y=[rnd(-10,10) for i in range(50)]
+z=[rnd(-5,5) for i in range(50)]
+c=["red","green"]
+plt.figure(figsize=(20,5))
+plt.title("---")
+plt.bar(x,y,color=c,label="Gas Price")
+plt.plot(x,z,color="yellow",label="Oil Prce")
+plt.legend()
+plt.show()
+
+print("@@@@@@@@")
+def choose_point(num,_min,_max):
+    lines=[rnd(_min,_max) for i in range(num)]
+    return lines
+x=choose_point(50,-10,10)
+y=choose_point(50,-6,3)
+_x=choose_point(50,-8,9)
+_x=choose_point(50,-1,10)
+plt.title("Scatterr Graph\n",color="purple")
+plt.scatter(x,y,c=choose_point(50,100,500),cmap="viridis",s=choose_point(50,100,150),edgecolors="black",linewidths=1.0,label="Oil Prices",alpha=0.7)#alpha for taransparany
+plt.scatter(x,y,c=choose_point(50,0,20),marker="*",cmap="viridis",s=choose_point(50,30,50),label="Oil Prices",linewidths=2.0,alpha=0.7)#alpha for taransparany
+cb=plt.colorbar()
+cb.set_label("Car_Price")
+plt.legend()
+plt.show()
+
+print("*********")
+labels=["Python","C++","Go","Php","Java","Rust"]
+vals=[30,20,30,14,20,18]
+cls=["gold","yellowgreen","teal","salmon","plum","red"]
+plt.pie(vals,labels=labels,colors=cls,startangle=90,autopct="%2.2f%%")
+plt.show()
+
+print("^^^^^^^^^")
+vals=[10,10,10,10,10,10]
+labels=["Python","C++","Go","Php","Java","Rust"]
+cls=["gold","yellowgreen","teal","salmon","plum","red"]
+plt.pie(vals,labels=labels,colors=cls,autopct="%2.0f%%",hatch=['//', '\\\\', '||', '--', '++', 'xx', 'oo', 'OO', '..', '**'])#hatch is setting for pattern
+plt.pie([100],colors=["white"],radius=0.8)
+plt.show()
+
+print("$$$$$$$$$$")
+vals=[60,50,40,30,10,9,8,7,6]
+labels=["Python","C++","Go","Php","Java","Rust","","",""]
+cls=["gold","yellowgreen","teal","salmon","plum","red","silver","navy","tan"]
+plt.pie(vals,labels=labels,colors=cls,autopct="%2.0f%%",explode=[0,0,0,0.1,0.2,0.3,0.4,0.5,0.6])
+plt.savefig("./new_pig.png")
+plt.show()
